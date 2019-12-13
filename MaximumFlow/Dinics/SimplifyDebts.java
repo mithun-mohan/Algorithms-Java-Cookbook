c7994300b38040374b3790f95e08222f7c2602b1
Griff0ly0n <mithunmk93@gmail.com>
2019-12-14 00:36:30 +0530

    [Simplify Debts]: Refactored code and added comments

--- MaximumFlow/Dinics/SimplifyDebts.java
+++ MaximumFlow/Dinics/SimplifyDebts.java
@@ -23,9 +23,64 @@ public class SimplifyDebts {
    * Here Alice, Bob, Charlie, David, Ema, Fred and Gabe are represented by vertices from 0 to 6 respectively.
    */
   private static void createGraphForDebts() {
+    //  List of all people in the group
     String[] person = { "Alice", "Bob", "Charlie", "David", "Ema", "Fred", "Gabe"};
     int n = person.length;
-    Dinics solver = new Dinics(n);
+    //  Creating a graph with n vertices
+    Dinics solver = new Dinics(n, person);
+    //  Adding edges to the graph
+    solver = addAllTransactions(solver);
+
+    System.out.println();
+    System.out.println("Simplifying Debts...");
+    System.out.println("--------------------");
+    System.out.println();
+
+    //  Map to keep track of visited edges
+    visitedEdges = new HashSet<>();
+    Integer edgePos;
+
+    while((edgePos = getNonVisitedEdge(solver.getEdges())) != null) {
+      //  Force recomputation of subsequent flows in the graph
+      solver.recompute();
+      //  Set source and sink in the flow graph
+      Dinics.Edge firstEdge = solver.getEdges().get(edgePos);
+      solver.setSource(firstEdge.from);
+      solver.setSink(firstEdge.to);
+      //  Initialize the residual graph to be same as the given graph
+      List<Dinics.Edge>[] residualGraph = solver.getGraph();
+      List<Dinics.Edge> newEdges = new ArrayList<>();
+
+      for(List<Dinics.Edge> allEdges : residualGraph) {
+        for(Dinics.Edge edge : allEdges) {
+          long remainingFlow = ((edge.flow < 0) ? edge.capacity : (edge.capacity - edge.flow));
+          //  If there is capacity remaining in the graph, then add the remaining capacity as an edge
+          //  so that it can be used for optimizing other debts within the graph
+          if(remainingFlow > 0) {
+            newEdges.add(new Dinics.Edge(edge.from, edge.to, remainingFlow));
+          }
+        }
+      }
+
+      //  Get the maximum flow between the source and sink
+      long maxFlow = solver.getMaxFlow();
+      //  Mark the edge from source to sink as visited
+      int source = solver.getSource();
+      int sink = solver.getSink();
+      visitedEdges.add(getHashKeyForEdge(source, sink));
+      //  Create a new graph
+      solver = new Dinics(n, person);
+      //  Add edges having remaining capacity
+      solver.addEdges(newEdges);
+      //  Add an edge from source to sink in the new graph with obtained maximum flow as it's weight
+      solver.addEdge(source, sink, maxFlow);
+    }
+    //  Print the edges in the graph
+    solver.printEdges();
+    System.out.println();
+  }
+
+  private static Dinics addAllTransactions(Dinics solver) {
     //  Transactions made by Bob
     solver.addEdge(1, 2, 40);
     //  Transactions made by Charlie
@@ -40,52 +95,19 @@ public class SimplifyDebts {
     //  Transactions made by Gabe
     solver.addEdge(6, 1, 30);
     solver.addEdge(6, 3, 10);
-
-    System.out.println();
-    System.out.println("Simplifying Debts...");
-    System.out.println("--------------------");
-    System.out.println();
-
-    visitedEdges = new HashSet<>();
-    Integer edgePos;
-
-    while((edgePos = getNonVisitedEdge(solver.getEdges())) != null) {
-      solver.recompute();
-
-      Dinics.Edge firstEdge = solver.getEdges().get(edgePos);
-      solver.setSource(firstEdge.from);
-      solver.setSink(firstEdge.to);
-
-      List<Dinics.Edge>[] residualGraph = solver.getGraph();
-      List<Dinics.Edge> newEdges = new ArrayList<>();
-
-      for(List<Dinics.Edge> allEdges : residualGraph) {
-        for(Dinics.Edge edge : allEdges) {
-          long capacity = ((edge.flow < 0) ? edge.capacity : (edge.capacity - edge.flow));
-          if(capacity > 0) {
-            newEdges.add(new Dinics.Edge(edge.from, edge.to, capacity));
-          }
-        }
-      }
-
-      int source = solver.getSource();
-      int sink = solver.getSink();
-      long maxFlow = solver.getMaxFlow();
-      System.out.println(String.format("%s ----%s---> %s", person[source], maxFlow, person[sink]));
-      visitedEdges.add(getEdgeHashKey(source, sink));
-
-      solver = new Dinics(n);
-      solver.addEdges(newEdges);
-      solver.addEdge(source, sink, maxFlow);
-    }
-    System.out.println();
+    return solver;
   }
 
+  /**
+  * Get any non visited edge in the graph
+  * @param edges list of all edges in the graph
+  * @return index of a non visited edge
+  */
   private static Integer getNonVisitedEdge(List<Dinics.Edge> edges) {
     Integer edgePos = null;
     int curEdge = 0;
     for(Dinics.Edge edge : edges) {
-      if(!visitedEdges.contains(getEdgeHashKey(edge.from, edge.to))) {
+      if(!visitedEdges.contains(getHashKeyForEdge(edge.from, edge.to))) {
         edgePos = curEdge;
       }
       curEdge++;
@@ -93,7 +115,13 @@ public class SimplifyDebts {
     return edgePos;
   }
 
-  private static Long getEdgeHashKey(int u, int v) {
+  /**
+  * Get a unique hash key for a given edge
+  * @param u the starting vertex in the edge
+  * @param v the ending vertex in the edge
+  * @return a unique hash key
+  */
+  private static Long getHashKeyForEdge(int u, int v) {
     return u * OFFSET + v;
   }
 }
@@ -117,8 +145,8 @@ class Dinics extends NetworkFlowSolverBase {
    *
    * @param n - The number of nodes in the graph including source and sink nodes.
    */
-  public Dinics(int n) {
-    super(n);
+  public Dinics(int n, String[] vertexLabels) {
+    super(n, vertexLabels);
     level = new int[n];
   }
 
@@ -187,6 +215,7 @@ abstract class NetworkFlowSolverBase {
 
   public static class Edge {
     public int from, to;
+    public String fromLabel, toLabel;
     public Edge residual;
     public long flow, cost;
     public final long capacity, originalCost;
@@ -232,6 +261,7 @@ abstract class NetworkFlowSolverBase {
 
   protected boolean[] minCut;
   protected List<Edge>[] graph;
+  protected String[] vertexLabels;
   protected List<Edge> edges;
 
   // 'visited' and 'visitedToken' are variables used for graph sub-routines to
@@ -251,9 +281,10 @@ abstract class NetworkFlowSolverBase {
    *
    * @param n - The number of nodes in the graph including source and sink nodes.
    */
-  public NetworkFlowSolverBase(int n) {
+  public NetworkFlowSolverBase(int n, String[] vertexLabels) {
     this.n = n;
     initializeGraph();
+    assignLabelsToVertices(vertexLabels);
     minCut = new boolean[n];
     visited = new int[n];
     edges = new ArrayList<>();
@@ -263,6 +294,13 @@ abstract class NetworkFlowSolverBase {
   private void initializeGraph() {
     graph = new List[n];
     for (int i = 0; i < n; i++) graph[i] = new ArrayList<Edge>();
+  }
+
+  // Add labels to vertices in the graph.
+  private void assignLabelsToVertices(String[] vertexLabels) {
+    if(vertexLabels.length != n)
+      throw new IllegalArgumentException(String.format("You must pass %s number of labels", n));
+    this.vertexLabels = vertexLabels;
   }
 
   /**
@@ -389,7 +427,7 @@ abstract class NetworkFlowSolverBase {
   }
 
   /**
-   * Set 'solved' flag to false to force recomputation.
+   * Set 'solved' flag to false to force recomputation of subsequent flows.
    */
   public void recompute() {
     solved = false;
@@ -399,13 +437,9 @@ abstract class NetworkFlowSolverBase {
    * Print all edges.
    */
   public void printEdges() {
-    System.out.println("List of Edges in the flow network...");
-    System.out.println("------------------------------------");
-    System.out.println();
     for(Edge edge : edges) {
-      System.out.println(String.format("%s-----%s----->%s", edge.from, edge.capacity, edge.to));
+      System.out.println(String.format("%s ----%s----> %s", vertexLabels[edge.from], edge.capacity, vertexLabels[edge.to]));
     }
-    System.out.println();
   }
 
   // Wrapper method that ensures we only call solve() once
